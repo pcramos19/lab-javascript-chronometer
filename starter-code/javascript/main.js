f0a391551e55b7b82faf0db3678a6e330ab3e5a4
Pedro Castañeda <pcramos19@gmail.com>
2020-01-21 17:37:49 +0100

    funcionan los btn

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,60 +1,59 @@
 let chronometer = new Chronometer();
-let btnLeft     = document.getElementById('btnLeft');
-let btnRight    = document.getElementById('btnRight');
-let minDec      = document.getElementById('minDec');
-let minUni      = document.getElementById('minUni');
-let secDec      = document.getElementById('secDec');
-let secUni      = document.getElementById('secUni');
-let milDec      = document.getElementById('milDec');
-let milUni      = document.getElementById('milUni');
+let btnLeft = document.getElementById("btnLeft");
+let btnRight = document.getElementById("btnRight");
+let minDec = document.getElementById("minDec");
+let minUni = document.getElementById("minUni");
+let secDec = document.getElementById("secDec");
+let secUni = document.getElementById("secUni");
+let milDec = document.getElementById("milDec");
+let milUni = document.getElementById("milUni");
 
+function printTime() {}
 
-function printTime() {
+function printMinutes() {}
 
-}
+function printSeconds() {}
 
-function printMinutes() {
+function printMilliseconds() {}
 
-}
+function printSplit() {}
 
-function printSeconds() {
-
-}
-
-function printMilliseconds() {
-
-}
-
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
+function clearSplits() {}
 
 function setStopBtn() {
-
+  btnLeft.innerText = `START`;
+  btnLeft.className = `btn start`;
 }
 
 function setSplitBtn() {
-
+    btnRight.innerText = `RESET`;
+    btnRight.className = `btn reset`;
 }
 
 function setStartBtn() {
-
+    btnLeft.innerText = `STOP`;
+    btnLeft.className = `btn stop`;
 }
 
 function setResetBtn() {
-
+    btnRight.innerText = `SPLIT`;
+    btnRight.className = `btn split`;
 }
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-
+btnLeft.addEventListener("click", function() {
+  if (btnLeft.innerText === `START`){
+      setStartBtn()
+  } else {
+      setStopBtn()
+  }
 });
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
-
+btnRight.addEventListener("click", function() {
+    if (btnRight.innerText === `RESET`){
+        setResetBtn();
+    } else {
+        setSplitBtn();
+    }
 });
